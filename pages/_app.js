3fcb91bc3e12c123b5db781376f840c5086a7076
Rohit Dhas <rohitdhas666@gmail.com>
2022-10-10 21:08:25 +0530

    implemented google auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
-import Layout from '../components/layout/Layout';
-import '../styles/globals.css'
+import Layout from "../components/layout/Layout";
+import { SessionProvider } from "next-auth/react";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
